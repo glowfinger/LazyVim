9e3426720c77eaaeb6b52da9aa553c4613ae1908
Darren Lilley <darren@glowfinger.com>
2026-05-27 18:02:45 +0100

    Updated settings to match work

--- init.lua
+++ init.lua
@@ -1,2 +1,8 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local gdproject = io.open(vim.fn.getcwd() .. "/project.godot", "r")
+if gdproject then
+  io.close(gdproject)
+  vim.fn.serverstart("./godothost")
+end
